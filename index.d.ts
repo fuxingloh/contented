486c400c8fb87ba52fce3000aeafb57673880b81
Fuxing Loh <git@fuxing.dev>
2022-07-14 00:33:11 +0800

    inject contentedConfig from env

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export interface ContentedConfig {
+  url: string;
+  name: string;
+  github_url: string;
+  types: string[];
+}
--- packages/contented-preview/next.config.js
+++ packages/contented-preview/next.config.js
@@ -1,19 +1,18 @@
-const { withContentlayer } = require('next-contentlayer');
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {ContentedConfig} */
+const contentedConfig = JSON.parse(process.env.CONTENTED_CONFIG);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts'],
+  pageExtensions: ["page.tsx", "page.ts"],
   swcMinify: true,
   env: {
-    SITE_URL: 'https://contented.dev',
-    SITE_NAME: 'Contented Documentation',
-    GITHUB_URL: 'https://github.com/BirthdayResearch/contented',
+    SITE_URL: contentedConfig.url,
+    SITE_NAME: contentedConfig.name,
+    GITHUB_URL: contentedConfig.github_url,
   },
 };
 
 module.exports = withContentlayer(nextConfig);
-
-function parseContentedConfig() {
-
-}

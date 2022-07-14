040d3018e866afb7b87a04e435444c407f4b6391
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2022-07-14 15:20:38 +0800

    feat(netlify): publish contented-example to netlify (#6)

--- packages/contented-example/docs/01:overview.md
+++ packages/contented-example/docs/01:overview.md
@@ -1,1 +1,3 @@
-# Contented ABC
+## [@birthdayresearch/contented](https://contented.dev)
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/de626542-ae6c-4204-bb85-334ae50fbb75/deploy-status)](https://app.netlify.com/sites/contented/deploys)
--- packages/contented-preview/cli.js
+++ packages/contented-preview/cli.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 const { symlinkSync, existsSync, rmSync, cpSync } = require('node:fs');
-const { spawn } = require('node:child_process');
+const { spawnSync } = require('node:child_process');
 const commandLineArgs = require('command-line-args');
 
 const mainDefinitions = [{ name: 'command', defaultOption: true }];
@@ -15,7 +15,7 @@ if (existsSync(path)) {
 }
 symlinkSync(target, path, 'file');
 
-spawn(`npm`, ['run', command, '--prefix', __dirname], {
+spawnSync(`npm`, ['run', command, '--prefix', __dirname], {
   stdio: 'inherit',
   cwd: __dirname,
   env: {
@@ -26,6 +26,16 @@ spawn(`npm`, ['run', command, '--prefix', __dirname], {
 
 if (command === 'build') {
   cpSync(`${__dirname}/.contentlayer/generated`, `${process.cwd()}/dist`, {
+    recursive: true,
+  });
+}
+
+if (command === 'generate') {
+  cpSync(`${__dirname}/.next`, `${process.cwd()}/.next`, {
+    recursive: true,
+  });
+
+  cpSync(`${__dirname}/out`, `${process.cwd()}/out`, {
     recursive: true,
   });
 }

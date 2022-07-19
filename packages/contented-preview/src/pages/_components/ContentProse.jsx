93172d48735ca335274f7a00321e5ecf9deaac2f
Fuxing Loh <git@fuxing.dev>
2022-07-19 18:32:20 +0800

    chore(contented-preview): update inline code block style

--- packages/contented-preview/src/pages/_components/ContentProse.jsx
+++ packages/contented-preview/src/pages/_components/ContentProse.jsx
@@ -14,6 +14,8 @@ export default function ContentProse({ as: Component = 'div', className, ...prop
         'prose-a:font-semibold dark:prose-a:text-primary-400',
         // link underline
         'prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,2px)+2px))_0_0_var(--tw-prose-underline,theme(colors.primary.300))] hover:prose-a:[--tw-prose-underline-size:4px] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.primary.800))] dark:hover:prose-a:[--tw-prose-underline-size:4px] dark:[--tw-prose-background:theme(colors.slate.900)]',
+        // code
+        'prose-code:before:content-none prose-code:after:content-none',
         // pre
         'prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10',
         // hr

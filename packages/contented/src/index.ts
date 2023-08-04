41a51cd8d25d823b9b33f7e79b4eb449562d847c
Fuxing Loh <git@fuxing.dev>
2023-08-04 11:44:05 +0800

    feat(contented): Programmatic API for Contented

--- packages/contented/src/index.ts
+++ packages/contented/src/index.ts
@@ -1,5 +1,10 @@
 import { Config as ProcessorConfig } from '@contentedjs/contented-processor';
 
+import { BuildCommand } from './commands/BuildCommand.js';
+import { GenerateCommand } from './commands/GenerateCommand.js';
+import { WatchCommand } from './commands/WatchCommand.js';
+import { WriteCommand } from './commands/WriteCommand.js';
+
 export * from '@contentedjs/contented-processor';
 
 export interface PreviewConfig {
@@ -14,3 +19,33 @@ export interface ContentedConfig {
   preview?: PreviewConfig;
   processor: ProcessorConfig;
 }
+
+/**
+ * Programmatic API for Contented
+ * To be used together in a website project like next.config.js.
+ */
+/* eslint-disable import/no-default-export */
+export default {
+  /**
+   * import contented from '@contentedjs/contented';
+   * await contented.build()
+   */
+  async build({ watch = process.env.NODE_ENV === 'development' } = {}): Promise<void> {
+    if (watch) {
+      await new BuildCommand().execute();
+    } else {
+      await new WatchCommand().execute();
+    }
+  },
+  /**
+   * import contented from '@contentedjs/contented';
+   * await contented.preview()
+   */
+  async preview({ watch = process.env.NODE_ENV === 'development' } = {}): Promise<void> {
+    if (watch) {
+      await new WriteCommand().execute();
+    } else {
+      await new GenerateCommand().execute();
+    }
+  },
+};

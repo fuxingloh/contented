ee7b48eee786c24c7fcfaa5cb8b3f1cfc933a360
Fuxing Loh <git@fuxing.dev>
2023-08-04 11:31:22 +0800

    chore(contented): use contented.config.mjs as default

--- packages/contented/src/commands/BaseCommand.ts
+++ packages/contented/src/commands/BaseCommand.ts
@@ -1,3 +1,4 @@
+import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 
 import { Command } from 'clipanion';
@@ -7,8 +8,18 @@ import { ContentedConfig } from '../index.js';
 export abstract class BaseCommand extends Command {
   static paths = [[`build`]];
 
+  /**
+   * Load the configuration file for Contented.
+   * Checks for `contented.config.mjs` or `contented.config.js` in the current working directory.
+   *
+   * @returns {Promise<ContentedConfig>} A promise that resolves to the loaded configuration object.
+   */
   async loadConfig(): Promise<ContentedConfig> {
-    const configPath = join(process.cwd(), 'contented.config.js');
+    let configPath = join(process.cwd(), 'contented.config.mjs');
+    if (!existsSync(configPath)) {
+      configPath = join(process.cwd(), 'contented.config.js');
+    }
+
     // eslint-disable-next-line no-console
     console.log(`Loading config from: ${configPath}`);
     const config = await import(configPath);

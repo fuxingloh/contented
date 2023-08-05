10ed909e57a76e787dc39a61f49f1e4d9bb84f4a
Fuxing Loh <git@fuxing.dev>
2023-08-05 12:56:49 +0800

    use client redirect

--- packages/contented-preview/src/pages/index.page.jsx
+++ packages/contented-preview/src/pages/index.page.jsx
@@ -1,13 +1,13 @@
 import { Index } from '../../../index.js';
-
-export async function getStaticProps({ params }) {
-  return {
-    redirect: {
-      destination: `/${Index[0].type.toLowerCase()}${Index[0].path}`,
-    },
-  };
-}
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function IndexPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(`/${Index[0].type.toLowerCase()}${Index[0].path}`);
+  }, []);
+
   return <></>;
 }

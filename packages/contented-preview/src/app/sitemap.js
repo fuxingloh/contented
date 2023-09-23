6d378e76449dd6c604107356b48a4a1920d6cf62
Fuxing Loh <git@fuxing.dev>
2023-09-23 14:54:42 +0800

    fix export

--- packages/contented-preview/src/app/sitemap.js
+++ packages/contented-preview/src/app/sitemap.js
@@ -2,11 +2,11 @@ import { Index } from '../../../index.js';
 
 export default function sitemap() {
   return [
-    ...Index.map((fileIndex) => {
+    ...Index.map((file) => {
       const path = `/${file.type.toLowerCase()}${file.path}`
       return {
         url: `${process.env.CONTENTED_PREVIEW_SITE_URL}/${path}`,
-        lastModified: new Date(fileIndex.modifiedDate),
+        lastModified: new Date(file.modifiedDate),
         changeFrequency: 'weekly',
       };
     })

3e871f2bf99ec2b70cb34e3b1121099dcbbbe883
Fuxing Loh <git@fuxing.dev>
2023-10-20 15:03:06 +0800

    chore(workspace): reducing project complexity

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  trailingComma: 'all',
+  singleQuote: true,
+  plugins: [import('prettier-plugin-tailwindcss'), import('prettier-plugin-packagejson')],
+};
+
+export default config;

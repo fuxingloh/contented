3dbae3f1b84180ea2308c9d891cb86c49d2cf165
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2024-02-14 10:33:08 +0800

    fix(contented): programmatic execution (#795)
    #### What this PR does / why we need it:
    
    As per title.

--- packages/contented/src/cli.ts
+++ packages/contented/src/cli.ts
@@ -3,8 +3,7 @@ import { runExit } from 'clipanion';
 
 import { BuildCommand } from './commands/BuildCommand.js';
 import { GenerateCommand } from './commands/GenerateCommand.js';
-import { WatchCommand } from './commands/WatchCommand.js';
 import { WriteCommand } from './commands/WriteCommand.js';
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-runExit([BuildCommand, GenerateCommand, WatchCommand, WriteCommand]);
+runExit([BuildCommand, GenerateCommand, WriteCommand]);
